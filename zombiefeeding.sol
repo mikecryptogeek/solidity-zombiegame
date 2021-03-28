2b097daa76f1d503e430e56c784c8956af29e54d
mikecryptogeek <cmdmjin@gmail.com>
2021-03-28 01:32:25 -0500

    create new Zombie by feeding kitties

--- zombiefeeding.sol
+++ zombiefeeding.sol
@@ -3,23 +3,23 @@ pragma solidity >=0.5.0 <0.6.0;
 import "./zombiefactory.sol";
 
 contract KittyInterface {
-    function getKitty(uint256 _id) external view returns (
-        bool isGestating,
-        bool isReady,
-        uint256 cooldownIndex,
-        uint256 nextActionAt,
-        uint256 siringWithId,
-        uint256 birthTime,
-        uint256 matronId,
-        uint256 sireId,
-        uint256 generation,
-        uint256 genes
-    );
+  function getKitty(uint256 _id) external view returns (
+    bool isGestating,
+    bool isReady,
+    uint256 cooldownIndex,
+    uint256 nextActionAt,
+    uint256 siringWithId,
+    uint256 birthTime,
+    uint256 matronId,
+    uint256 sireId,
+    uint256 generation,
+    uint256 genes
+  );
 }
 
 contract ZombieFeeding is ZombieFactory {
 
-  address ckAddress = 2c8cf97B0x0601EaD5deAe237070F9587f8E7A266d;
+  address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d;
   KittyInterface kittyContract = KittyInterface(ckAddress);
 
   function feedAndMultiply(uint _zombieId, uint _targetDna) public {
@@ -27,7 +27,14 @@ contract ZombieFeeding is ZombieFactory {
     Zombie storage myZombie = zombies[_zombieId];
     _targetDna = _targetDna % dnaModulus;
     uint newDna = (myZombie.dna + _targetDna) / 2;
+    // Add an if statement here
     _createZombie("NoName", newDna);
+  }
+
+  function feedOnKitty(uint _zombieId, uint _kittyId) public {
+    uint kittyDna;
+    (,,,,,,,,,kittyDna) = kittyContract.getKitty(_kittyId);
+    feedAndMultiply(_zombieId, kittyDna);
   }
 
 }
